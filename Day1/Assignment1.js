d9b4d8e82e0eda6fe7d3cacd581ea2e68a5d5de8
Charan-47 <62457412+Charan-47@users.noreply.github.com>
2020-09-03 21:13:01 +0530

    Add files via upload

---
+++ Day1/Assignment1.js
@@ -0,0 +1,35 @@
+//question 1:
+
+let data1 = "Hello Javascript";
+console.log(data1[6]);
+
+//question 2:
+
+let time = 60;
+console.log(time*60);
+
+//question 3:
+
+let data2 = [123,678,345,765,107];
+console.log(data2[3]);
+
+//question 4:
+let data3 = ["ate","eat","ant","tea","aea"];
+for(let i=0;i<=4;i++)
+{
+    if(data3[i][0] == "a")
+    {
+        console.log(data3[i]);
+    }
+}
+
+//question 5:
+
+let data4 = [123,678,345,765,107];
+for(let i=data4.length-1;i>=0;i--)
+{
+    console.log(data4[i]);
+}
+
+
+
