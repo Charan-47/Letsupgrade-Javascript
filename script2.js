d5dd0b346359f652b8633d1d2d5050b503e0d9ec
Charan-47 <62457412+Charan-47@users.noreply.github.com>
2021-01-23 14:38:45 +0530

    Assignment 2

---
+++ script2.js
@@ -0,0 +1,17 @@
+
+
+
+const clothes = ["jacket","t-shirt"]
+clothes.length = 0
+console.log(clothes[0])
+
+//undefined ,it is because array length is made zero
+
+
+
+var arr = [1,2,3,4,5,6,7]
+sum=0;
+for(var i=0;i<arr[i];i++)
+{
+    sum = sum +arr[i];
+}
